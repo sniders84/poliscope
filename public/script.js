7a1b3984f49047443d03b9881e2ce5706fc80520
sniders84 <sniders1984@gmail.com>
2025-09-24 03:04:40 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,13 +1,19 @@
 async function loadData() {
-  const house = await fetch('data/house.json').then(res => res.json())
+  try {
+    const house = await fetch('./data/House.json').then(res => res.json())
+    const governors = await fetch('./data/Governors.json').then(res => res.json())
+    const senate = await fetch('./data/Senate.json').then(res => res.json())
 
-  const governors = await fetch('data/governors.json').then(res => res.json())
+    const houseList = house.map(rep => `<li>${rep.name} (${rep.state})</li>`).join('')
+    const govList = governors.map(gov => `<li>${gov.name} (${gov.state})</li>`).join('')
+    const senateList = senate.map(sen => `<li>${sen.name} (${sen.state})</li>`).join('')
 
-  const houseList = house.map(rep => `<li>${rep.name} (${rep.state})</li>`).join('')
-  const govList = governors.map(gov => `<li>${gov.name} (${gov.state})</li>`).join('')
-
-  document.getElementById('house').innerHTML = `<h2>House</h2><ul>${houseList}</ul>`
-  document.getElementById('governors').innerHTML = `<h2>Governors</h2><ul>${govList}</ul>`
+    document.getElementById('house').innerHTML = `<h2>House</h2><ul>${houseList}</ul>`
+    document.getElementById('governors').innerHTML = `<h2>Governors</h2><ul>${govList}</ul>`
+    document.getElementById('senate').innerHTML = `<h2>Senate</h2><ul>${senateList}</ul>`
+  } catch (err) {
+    console.error("Error loading data:", err)
+  }
 }
 
 loadData()
