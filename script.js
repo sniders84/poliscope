758dc72e94bc66052ad5a8d146bda7bda5cd5139
sniders84 <sniders1984@gmail.com>
2025-09-24 02:41:37 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+async function loadData() {
+  const house = await fetch('data/house.json').then(res => res.json())
+  const governors = await fetch('data/governors.json').then(res => res.json())
+
+  const houseList = house.map(rep => `<li>${rep.name} (${rep.state})</li>`).join('')
+  const govList = governors.map(gov => `<li>${gov.name} (${gov.state})</li>`).join('')
+
+  document.getElementById('house').innerHTML = `<h2>House</h2><ul>${houseList}</ul>`
+  document.getElementById('governors').innerHTML = `<h2>Governors</h2><ul>${govList}</ul>`
+}
+
+loadData()
