8cf59cfca34f946cce176cf50c1a6529724ccb8f
sniders84 <sniders1984@gmail.com>
2025-10-01 02:51:29 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,10 @@
+function showTab(tabId) {
+  const tabs = document.querySelectorAll('.tab-content');
+  tabs.forEach(tab => tab.style.display = 'none');
+
+  const activeTab = document.getElementById(tabId);
+  if (activeTab) activeTab.style.display = 'block';
+}
 /* ---------------- GLOBAL DATA ---------------- */
 window.allOfficials = [];
 window.allStats = [];
