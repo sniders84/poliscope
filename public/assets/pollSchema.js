6fe82e141e4ae88566f6ab8e27e0194adfa71402
sniders84 <sniders1984@gmail.com>
2025-10-22 05:32:16 -0400

    Create pollSchema.js

---
+++ public/assets/pollSchema.js
@@ -0,0 +1,14 @@
+export const pollSchema = {
+  pollster: '',
+  date_range: '',
+  sample_size: 0,
+  method: '',
+  margin_of_error: '',
+  topic: '',
+  office: '',
+  jurisdiction: '',
+  results: {},
+  source_url: '',
+  ingested_at: '',
+  override: false
+}
