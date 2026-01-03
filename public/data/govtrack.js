67bb04b96d94477237b84ce8ebc568e7e90a3e44
sniders84 <sniders1984@gmail.com>
2026-01-03 05:02:13 -0500

    Create govtrack.js

---
+++ public/data/govtrack.js
@@ -0,0 +1,41 @@
+[
+  {
+    "id": 2024,
+    "name": "John Doe",
+    "roles": [
+      {
+        "bills_cosponsored": 15,
+        "bills_sponsored": 3,
+        "missed_votes_pct": 2.5,
+        "ideology_score": 45,
+        "leadership_score": 12
+      }
+    ]
+  },
+  {
+    "id": 2022,
+    "name": "Jane Smith",
+    "roles": [
+      {
+        "bills_cosponsored": 8,
+        "bills_sponsored": 5,
+        "missed_votes_pct": 1.2,
+        "ideology_score": 60,
+        "leadership_score": 20
+      }
+    ]
+  },
+  {
+    "id": 456796,
+    "name": "Alex Jones",
+    "roles": [
+      {
+        "bills_cosponsored": 20,
+        "bills_sponsored": 7,
+        "missed_votes_pct": 4.0,
+        "ideology_score": 30,
+        "leadership_score": 15
+      }
+    ]
+  }
+]
