447dca5aba77b917cd0db99697a48ee14e513449
sniders84 <sniders1984@gmail.com>
2026-01-09 02:46:04 -0500

    Update merge-senators.js

--- scripts/merge-senators.js
+++ scripts/merge-senators.js
@@ -1,38 +1,46 @@
 const fs = require('fs');
 
-const baseData = JSON.parse(fs.readFileSync('public/senators-base.json', 'utf8'));
+const baseData = JSON.parse(fs.readFileSync('public/senators-rankings.json', 'utf8'));
 const legislationData = JSON.parse(fs.readFileSync('public/senators-legislation.json', 'utf8'));
 const committeesData = JSON.parse(fs.readFileSync('public/senators-committees.json', 'utf8'));
 const votesData = JSON.parse(fs.readFileSync('public/senators-votes.json', 'utf8'));
 
 const jsonPath = 'public/senators-rankings.json';
 
-function mergeSenatorRecords(base, legislation, committees, votes) {
+function mergeSenatorData(base, legislation, committees, votes) {
+  const leg = legislation.find(l => l.bioguideId === base.bioguideId) || {};
+  const com = committees.find(c => c.bioguideId === base.bioguideId) || {};
+  const vot = votes.find(v => v.bioguideId === base.bioguideId) || {};
+
   return {
     name: base.name,
     state: base.state,
     party: base.party,
     office: base.office,
     bioguideId: base.bioguideId,
-    sponsoredBills: legislation?.sponsoredBills || 0,
-    sponsoredAmendments: legislation?.sponsoredAmendments || 0,
-    cosponsoredBills: legislation?.cosponsoredBills || 0,
-    cosponsoredAmendments: legislation?.cosponsoredAmendments || 0,
-    becameLawBills: legislation?.becameLawBills || 0,
-    becameLawAmendments: legislation?.becameLawAmendments || 0,
-    committees: committees?.committees || [],
-    missedVotes: votes?.missedVotes || 0
+
+    // Legislation
+    sponsoredBills: leg.sponsoredBills || 0,
+    sponsoredAmendments: leg.sponsoredAmendments || 0,
+    cosponsoredBills: leg.cosponsoredBills || 0,
+    cosponsoredAmendments: leg.cosponsoredAmendments || 0,
+    becameLawBills: leg.becameLawBills || 0,
+    becameLawAmendments: leg.becameLawAmendments || 0,
+    becameLawCosponsoredAmendments: leg.becameLawCosponsoredAmendments || 0,
+
+    // Committees
+    committees: com.committees || [],
+
+    // Votes
+    missedVotes: vot.missedVotes || 0
   };
 }
 
 (async () => {
-  const merged = baseData.map(base => {
-    const legislation = legislationData.find(l => l.bioguideId === base.bioguideId);
-    const committees = committeesData.find(c => c.bioguideId === base.bioguideId);
-    const votes = votesData.find(v => v.bioguideId === base.bioguideId);
-    return mergeSenatorRecords(base, legislation, committees, votes);
-  });
+  const output = baseData.map(base =>
+    mergeSenatorData(base, legislationData, committeesData, votesData)
+  );
 
-  fs.writeFileSync(jsonPath, JSON.stringify(merged, null, 2) + '\n');
+  fs.writeFileSync(jsonPath, JSON.stringify(output, null, 2) + '\n');
   console.log('senators-rankings.json fully merged and updated!');
 })();
