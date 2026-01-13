52ee3b5233e87778f342982fd8894433e256b2c1
sniders84 <sniders1984@gmail.com>
2026-01-13 03:17:45 -0500

    Update votes-scraper.js

--- scripts/votes-scraper.js
+++ scripts/votes-scraper.js
@@ -1,13 +1,12 @@
 // votes-scraper.js
 // Scrapes Senate.gov roll call XML indexes
-// Outputs senators-votes.json
+// Outputs public/senators-votes.json
 
 const fs = require('fs');
 const fetch = require('node-fetch');
 const xml2js = require('xml2js');
 
-const ROLL_CALL_BASE = 'https://www.senate.gov/legislative/LIS/roll_call_votes/vote119'; 
-// Example: vote1191.xml, vote1192.xml etc. (1st and 2nd sessions)
+const ROLL_CALL_BASE = 'https://www.senate.gov/legislative/LIS/roll_call_votes/vote119';
 
 async function fetchRollCall(congress, session, voteNumber) {
   const url = `${ROLL_CALL_BASE}${session}/vote_${congress}_${session}_${voteNumber}.xml`;
@@ -18,12 +17,11 @@ async function fetchRollCall(congress, session, voteNumber) {
 }
 
 async function scrapeVotes() {
-  const senators = JSON.parse(fs.readFileSync('senators.json', 'utf8'));
+  const senators = JSON.parse(fs.readFileSync('public/senators.json', 'utf8'));
   const voteData = {};
 
-  // Loop through sessions (1 and 2) and vote numbers
   for (let session = 1; session <= 2; session++) {
-    for (let voteNum = 1; voteNum <= 1000; voteNum++) { // upper bound, break when no file
+    for (let voteNum = 1; voteNum <= 1000; voteNum++) {
       const data = await fetchRollCall(119, session, voteNum);
       if (!data) break;
 
@@ -58,7 +56,7 @@ async function scrapeVotes() {
     };
   });
 
-  fs.writeFileSync('senators-votes.json', JSON.stringify(output, null, 2));
+  fs.writeFileSync('public/senators-votes.json', JSON.stringify(output, null, 2));
   console.log('Votes scraper complete!');
 }
 
