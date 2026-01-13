efd40badc3906230e60d07164c1bb9194025f9a7
sniders84 <sniders1984@gmail.com>
2026-01-13 00:37:54 -0500

    Update senators-legislation.js

--- scripts/senators-legislation.js
+++ scripts/senators-legislation.js
@@ -4,30 +4,70 @@ const fetch = require('node-fetch');
 const CONGRESS_API_KEY = process.env.CONGRESS_API_KEY;
 const base = JSON.parse(fs.readFileSync('public/senators-rankings.json', 'utf8'));
 
+async function fetchAllLegislation(urlBase, key) {
+  const pageSize = 250;
+  let offset = 0;
+  let all = [];
+  while (true) {
+    const url = `${urlBase}?limit=${pageSize}&offset=${offset}&api_key=${CONGRESS_API_KEY}`;
+    const res = await fetch(url);
+    if (!res.ok) break;
+    const data = await res.json();
+    const items = data[key] || [];
+    all = all.concat(items);
+    if (items.length < pageSize) break;
+    offset += pageSize;
+  }
+  return all;
+}
+
 async function getLegislation(bioguideId) {
   try {
-    const url = `https://api.congress.gov/v3/member/${bioguideId}/sponsored-legislation?limit=250&api_key=${CONGRESS_API_KEY}`;
-    const res = await fetch(url);
-    if (!res.ok) return {};
+    const sponsoredItems = await fetchAllLegislation(
+      `https://api.congress.gov/v3/member/${bioguideId}/sponsored-legislation`,
+      'sponsoredLegislation'
+    );
+    const cosponsoredItems = await fetchAllLegislation(
+      `https://api.congress.gov/v3/member/${bioguideId}/cosponsored-legislation`,
+      'cosponsoredLegislation'
+    );
 
-    const data = await res.json();
-    const items = data.sponsoredLegislation || [];
+    const sponsoredLegislation = sponsoredItems.filter(l => l.type !== 'amendment').length;
+    const sponsoredAmendments = sponsoredItems.filter(l => l.type === 'amendment').length;
 
-    const sponsoredLegislation = items.filter(l => l.type !== 'amendment').length;
-    const sponsoredAmendments = items.filter(l => l.type === 'amendment').length;
+    const cosponsoredLegislation = cosponsoredItems.filter(l => l.type !== 'amendment').length;
+    const cosponsoredAmendments = cosponsoredItems.filter(l => l.type === 'amendment').length;
 
-    // TODO: add cosponsored endpoint calls if needed
-    const cosponsoredLegislation = 0;
-    const cosponsoredAmendments = 0;
+    const becameLawLegislation = sponsoredItems.filter(l =>
+      /became law|public law/i.test(l.latestAction?.actionDescription || '')
+    ).length;
+    const becameLawAmendments = sponsoredItems.filter(l =>
+      l.type === 'amendment' && /agreed to|became law|public law/i.test(l.latestAction?.actionDescription || '')
+    ).length;
+    const becameLawCosponsoredAmendments = cosponsoredItems.filter(l =>
+      l.type === 'amendment' && /agreed to|became law|public law/i.test(l.latestAction?.actionDescription || '')
+    ).length;
 
-    const becameLawLegislation = items.filter(l => /Became Public Law/i.test(l.latestAction?.actionDescription || '')).length;
-    const becameLawAmendments = items.filter(l => l.type === 'amendment' && /Became Public Law/i.test(l.latestAction?.actionDescription || '')).length;
-    const becameLawCosponsoredAmendments = 0;
-
-    return { sponsoredLegislation, sponsoredAmendments, cosponsoredLegislation, cosponsoredAmendments, becameLawLegislation, becameLawAmendments, becameLawCosponsoredAmendments };
+    return {
+      sponsoredLegislation,
+      sponsoredAmendments,
+      cosponsoredLegislation,
+      cosponsoredAmendments,
+      becameLawLegislation,
+      becameLawAmendments,
+      becameLawCosponsoredAmendments
+    };
   } catch (err) {
     console.error(`Error for ${bioguideId}: ${err.message}`);
-    return {};
+    return {
+      sponsoredLegislation: 0,
+      sponsoredAmendments: 0,
+      cosponsoredLegislation: 0,
+      cosponsoredAmendments: 0,
+      becameLawLegislation: 0,
+      becameLawAmendments: 0,
+      becameLawCosponsoredAmendments: 0
+    };
   }
 }
 
@@ -35,7 +75,7 @@ async function main() {
   for (const sen of base) {
     const data = await getLegislation(sen.bioguideId);
     Object.assign(sen, data);
-    console.log(`Updated ${sen.name}: sponsoredLegislation ${data.sponsoredLegislation || 0}`);
+    console.log(`Updated ${sen.name}: sponsoredLegislation ${data.sponsoredLegislation}, cosponsoredLegislation ${data.cosponsoredLegislation}`);
   }
   fs.writeFileSync('public/senators-rankings.json', JSON.stringify(base, null, 2));
   console.log('senators-rankings.json updated with legislation!');
