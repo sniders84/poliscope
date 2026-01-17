1a22d376b0a6136d9a60a0ad3380d3b989c01df2
sniders84 <sniders1984@gmail.com>
2026-01-17 05:29:05 -0500

    Update merge-reps.js

--- scripts/merge-reps.js
+++ scripts/merge-reps.js
@@ -1,14 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 
-const legislation = JSON.parse(fs.readFileSync(path.join(__dirname, '../public/house-legislation.json')));
-const votes = JSON.parse(fs.readFileSync(path.join(__dirname, '../public/house-votes.json')));
-const rankings = JSON.parse(fs.readFileSync(path.join(__dirname, '../public/representatives-rankings.json')));
+const rankingsPath = path.join(__dirname, '../public/representatives-rankings.json');
+const bills = JSON.parse(fs.readFileSync(path.join(__dirname, '../public/house-legislation.json'))).bills || [];
+const votes = JSON.parse(fs.readFileSync(path.join(__dirname, '../public/house-votes.json'))).votes || [];
+const rankings = JSON.parse(fs.readFileSync(rankingsPath));
 
-rankings.forEach(rep => {
-  rep.legislation = legislation.bills?.filter(b => b.sponsor?.bioguideId === rep.bioguideId) || [];
-  rep.votes = votes.votes?.filter(v => v.member?.bioguideId === rep.bioguideId) || [];
-});
+const bySponsor = new Map();
+for (const b of bills) {
+  const sponsorId = b.sponsor?.bioguideId || b.sponsor?.bioguide_id || null;
+  if (!sponsorId) continue;
+  if (!bySponsor.has(sponsorId)) bySponsor.set(sponsorId, []);
+  bySponsor.get(sponsorId).push(b);
+}
 
-fs.writeFileSync(path.join(__dirname, '../public/representatives-rankings.json'), JSON.stringify(rankings, null, 2));
+const votesByMember = new Map();
+for (const v of votes) {
+  const memberId = v.member?.bioguideId || v.member?.bioguide_id || null;
+  if (!memberId) continue;
+  if (!votesByMember.has(memberId)) votesByMember.set(memberId, []);
+  votesByMember.get(memberId).push(v);
+}
+
+for (const rep of rankings) {
+  const id = rep.bioguideId || rep.bioguide_id;
+  rep.legislation = bySponsor.get(id) || [];
+  rep.votes = votesByMember.get(id) || [];
+}
+
+fs.writeFileSync(rankingsPath, JSON.stringify(rankings, null, 2));
 console.log('Merged House legislation and votes into representatives-rankings.json');
