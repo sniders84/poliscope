67347d2ecae14161882136d273c15cb3bd4825d5
sniders84 <sniders1984@gmail.com>
2026-01-20 19:21:36 -0500

    Update merge-representatives.js

--- scripts/merge-representatives.js
+++ scripts/merge-representatives.js
@@ -1,54 +1,33 @@
 // scripts/merge-representatives.js
-// Purpose: Merge bootstrap, legislation, committees, votes into representatives-rankings.json
-// Amendment fields removed
+//
+// Purpose: Merge legislation data from legislation-representatives.json into representatives-rankings.json
+// Output: updated public/representatives-rankings.json
 
 const fs = require('fs');
 const path = require('path');
 
-const bootstrapPath = path.join(__dirname, '../public/representatives-rankings.json');
-const legislationPath = path.join(__dirname, '../public/legislation-representatives.json');
-const committeesPath = path.join(__dirname, '../public/committees-representatives.json');
-const votesPath = path.join(__dirname, '../public/votes-representatives.json');
+const rankingsPath = path.join(__dirname, '../public/representatives-rankings.json');
+const legisPath = path.join(__dirname, '../public/legislation-representatives.json');
 
-const reps = JSON.parse(fs.readFileSync(bootstrapPath, 'utf-8'));
-const legislation = JSON.parse(fs.readFileSync(legislationPath, 'utf-8'));
-const committees = JSON.parse(fs.readFileSync(committeesPath, 'utf-8'));
-const votes = JSON.parse(fs.readFileSync(votesPath, 'utf-8'));
+let rankings = JSON.parse(fs.readFileSync(rankingsPath, 'utf-8'));
+const legis = JSON.parse(fs.readFileSync(legisPath, 'utf-8'));
 
-function findLegislation(bioguideId) {
-  return legislation.find(l => l.bioguideId === bioguideId);
-}
-function findCommittees(bioguideId) {
-  return committees.find(c => c.bioguideId === bioguideId);
-}
-function findVotes(bioguideId) {
-  return votes.find(v => v.bioguideId === bioguideId);
-}
+// Build lookup by bioguideId
+const legisMap = new Map(legis.map(m => [m.bioguideId, m]));
 
-for (const rep of reps) {
-  const leg = findLegislation(rep.bioguideId);
-  if (leg) {
-    rep.sponsoredBills = leg.sponsoredBills;
-    rep.cosponsoredBills = leg.cosponsoredBills;
-    rep.becameLawBills = leg.becameLawBills;
-    rep.becameLawCosponsoredBills = leg.becameLawCosponsoredBills;
-  }
-
-  const com = findCommittees(rep.bioguideId);
-  if (com) {
-    rep.committees = com.committees;
-  }
-
-  const v = findVotes(rep.bioguideId);
-  if (v) {
-    rep.yeaVotes = v.yeaVotes;
-    rep.nayVotes = v.nayVotes;
-    rep.missedVotes = v.missedVotes;
-    rep.totalVotes = v.totalVotes;
-    rep.participationPct = v.participationPct;
-    rep.missedVotePct = v.missedVotePct;
+// Inject legislation data
+for (const rep of rankings) {
+  const legData = legisMap.get(rep.bioguideId);
+  if (legData) {
+    Object.assign(rep, {
+      sponsoredBills: legData.sponsoredBills,
+      cosponsoredBills: legData.cosponsoredBills,
+      becameLawBills: legData.becameLawBills,
+      becameLawCosponsoredBills: legData.becameLawCosponsoredBills,
+      lastUpdated: legData.lastUpdated
+    });
   }
 }
 
-fs.writeFileSync(bootstrapPath, JSON.stringify(reps, null, 2));
-console.log(`Merge complete: ${reps.length} representatives updated`);
+fs.writeFileSync(rankingsPath, JSON.stringify(rankings, null, 2));
+console.log(`Merged legislation data into representatives-rankings.json (${rankings.length} records)`);
