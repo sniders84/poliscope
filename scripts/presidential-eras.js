0fd22f6cf1371f5966e808135b3c3c16d14f3b41
sniders84 <sniders1984@gmail.com>
2026-02-27 19:40:04 -0500

    Update presidential-eras.js

--- scripts/presidential-eras.js
+++ scripts/presidential-eras.js
@@ -6,8 +6,7 @@ module.exports = {
   antebellum: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15], // JQ Adams → Buchanan
   civilWar: [16, 17, 18, 19],                    // Lincoln → Hayes
   industrial: [20, 21, 22, 23, 24, 25],          // Garfield → McKinley
-  progressive: [26, 27, 28, 29, 30, 31],         // T. Roosevelt → Hoover
-  depressionWWII: [32, 33],                      // FDR → Truman
+  progressive: [26, 27, 28, 29, 30, 31, 32, 33], // TR → Truman (merged Depression/WWII)
   coldWar: [34, 35, 36, 37, 38, 39, 40, 41],     // Eisenhower → George H.W. Bush
   postColdWar: [42, 43, 44, 45, 46]              // Clinton → Biden
 };
